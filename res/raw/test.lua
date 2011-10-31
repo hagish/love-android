cd65ec7784b0bb46fd89e2d3a472fce1663ae712
Sebastian Dorda <hagish@schattenkind.net>
2011-10-31 18:06:04 +0100

    first working lua example

---
+++ res/raw/test.lua
@@ -0,0 +1,5 @@
+function test()
+	print("hello world")
+	return "lala123"
+end
+
--- src/net/schattenkind/androidLove/LoveAndroidActivity.java
+++ src/net/schattenkind/androidLove/LoveAndroidActivity.java
@@ -1,7 +1,18 @@
 package net.schattenkind.androidLove;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.luaj.vm2.LoadState;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
 import android.app.Activity;
+import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class LoveAndroidActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +20,34 @@ public class LoveAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final TextView text = (TextView) this.findViewById(R.id.text);
+        
+        LuaValue _G = JsePlatform.standardGlobals();
+        
+        _G.set("print", new OneArgFunction() {
+
+            @Override
+            public LuaValue call(LuaValue luaValue) {
+            	text.setText(text.getText() + "\n" + luaValue.toString());
+            	Log.i("lua", luaValue.toString());
+            	
+                return LuaValue.NONE;
+            }
+        });
+        
+        try {
+			LoadState.load( getResources().openRawResource(R.raw.test), "test.lua", _G ).call();
+		} catch (NotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        LuaValue t = _G.get("test").call();
+        
+        text.setText(text.getText() + "\n" + t.toString());
     }
 }
