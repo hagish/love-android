203564629810fe39e4b10898ee1e97968813b7c2
Sebastian Dorda <hagish@schattenkind.net>
2011-10-31 17:15:01 +0100

    simple opengl demo

---
+++ src/net/schattenkind/androidLove/HelloOpenGLES10.java
@@ -0,0 +1,50 @@
+package net.schattenkind.androidLove;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class HelloOpenGLES10 extends Activity {
+  
+    private GLSurfaceView mGLView;
+  
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new HelloOpenGLES10SurfaceView(this);
+        setContentView(mGLView);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
+    }
+}
+  
+class HelloOpenGLES10SurfaceView extends GLSurfaceView {
+
+    public HelloOpenGLES10SurfaceView(Context context){
+        super(context);
+        
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(new HelloOpenGLES10Renderer());
+    }
+}
---
+++ src/net/schattenkind/androidLove/HelloOpenGLES10Renderer.java
@@ -0,0 +1,60 @@
+package net.schattenkind.androidLove;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView;
+
+public class HelloOpenGLES10Renderer implements GLSurfaceView.Renderer {
+	private FloatBuffer triangleVB;
+
+	private void initShapes() {
+
+		float triangleCoords[] = {
+				// X, Y, Z
+				-0.5f, -0.25f, 0, 0.5f, -0.25f, 0, 0.0f, 0.559016994f, 0 };
+
+		// initialize vertex Buffer for triangle
+		ByteBuffer vbb = ByteBuffer.allocateDirect(
+		// (# of coordinate values * 4 bytes per float)
+				triangleCoords.length * 4);
+		vbb.order(ByteOrder.nativeOrder());// use the device hardware's native
+											// byte order
+		triangleVB = vbb.asFloatBuffer(); // create a floating point buffer from
+											// the ByteBuffer
+		triangleVB.put(triangleCoords); // add the coordinates to the
+										// FloatBuffer
+		triangleVB.position(0); // set the buffer to read the first coordinate
+
+	}
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// Set the background frame color
+		gl.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+		// initialize the triangle vertex array
+		initShapes();
+
+		// Enable use of vertex arrays
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+	}
+
+	public void onDrawFrame(GL10 gl) {
+		// Redraw background color
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+		// Draw the triangle
+		gl.glColor4f(0.63671875f, 0.76953125f, 0.22265625f, 0.0f);
+		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, triangleVB);
+		gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
+	}
+
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glViewport(0, 0, width, height);
+	}
+
+}
