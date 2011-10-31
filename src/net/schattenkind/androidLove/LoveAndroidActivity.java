4fcf489fe62d8a9bdf8674ca2250f336a9ac573b
Sebastian Dorda <hagish@schattenkind.net>
2011-10-31 17:03:07 +0100

    initial empty project

---
+++ src/net/schattenkind/androidLove/LoveAndroidActivity.java
@@ -0,0 +1,13 @@
+package net.schattenkind.androidLove;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoveAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
