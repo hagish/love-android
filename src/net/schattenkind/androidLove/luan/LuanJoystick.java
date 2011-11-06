9a8db8fb6e79d39add89d43b028686de01edd2c4
ghoulsblade <ghoulsblade@schattenkind.net>
2011-11-06 03:36:04 +0100

    improved love.joystick dummy return values

--- src/net/schattenkind/androidLove/luan/LuanJoystick.java
+++ src/net/schattenkind/androidLove/luan/LuanJoystick.java
@@ -19,20 +19,20 @@ public class LuanJoystick extends LuanBase {
 		LuaTable t = LuaValue.tableOf();
 
 		// TODO: not yet implemented
-		t.set("close"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"close"			);	return LuaValue.NONE; } });	
-		t.set("getAxes"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getAxes"			);	return LuaValue.NONE; } });	
-		t.set("getAxis"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getAxis"			);	return LuaValue.NONE; } });	
-		t.set("getBall"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getBall"			);	return LuaValue.NONE; } });	
-		t.set("getHat"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getHat"			);	return LuaValue.NONE; } });	
+		t.set("close"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"close"				);	return LuaValue.NONE; } });	
+		t.set("getAxes"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getAxes"			);	return LuaValue.varargsOf(LuaValue.ZERO,LuaValue.ZERO,LuaValue.ZERO); } });	
+		t.set("getAxis"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getAxis"			);	return LuaValue.ZERO; } });	
+		t.set("getBall"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getBall"			);	return LuaValue.varargsOf(LuaValue.ZERO,LuaValue.ZERO); } });	
+		t.set("getHat"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getHat"			);	return LuaValue.ZERO; } });	
 		t.set("getName"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getName"			);	return LuaValue.NONE; } });	
-		t.set("getNumAxes"		, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumAxes"		);	return LuaValue.NONE; } });	
-		t.set("getNumBalls"		, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumBalls"		);	return LuaValue.NONE; } });	
-		t.set("getNumButtons"	, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumButtons"	);	return LuaValue.NONE; } });	
-		t.set("getNumHats"		, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumHats"		);	return LuaValue.NONE; } });	
-		t.set("getNumJoysticks"	, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumJoysticks"	);	return LuaValue.NONE; } });	
+		t.set("getNumAxes"		, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumAxes"		);	return LuaValue.ZERO; } });	
+		t.set("getNumBalls"		, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumBalls"		);	return LuaValue.ZERO; } });	
+		t.set("getNumButtons"	, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumButtons"		);	return LuaValue.ZERO; } });	
+		t.set("getNumHats"		, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumHats"		);	return LuaValue.ZERO; } });	
+		t.set("getNumJoysticks"	, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"getNumJoysticks"	);	return LuaValue.ZERO; } });	
 		t.set("isDown"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"isDown"			);	return LuaValue.NONE; } });	
 		t.set("isOpen"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"isOpen"			);	return LuaValue.NONE; } });	
-		t.set("open"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"open"			);	return LuaValue.NONE; } });	
+		t.set("open"			, new VarArgFunction() { @Override public Varargs invoke(Varargs args) { vm.NotImplemented("love.joystick."+"open"				);	return LuaValue.NONE; } });	
 
 		return t;
 	}
