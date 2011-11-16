ed26e29d56294600831b30a1d731ce1135fe8267
Sebastian Dorda <hagish@schattenkind.net>
2011-11-16 23:59:52 +0100

    ups forgot to add file

---
+++ src/net/schattenkind/androidLove/FileUtils.java
@@ -0,0 +1,12 @@
+package net.schattenkind.androidLove;
+
+public class FileUtils {
+	/**
+	 * @param filename eg. lala.txt
+	 * @return eg. txt (without leading .)
+	 */
+	public static String getFileExtension(String filename) {
+		String[] parts = filename.split("\\.");
+		return parts[parts.length - 1];
+	}
+}
